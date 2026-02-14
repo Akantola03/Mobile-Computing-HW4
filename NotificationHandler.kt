4ec2f46f63167d026dcf58527665e639c0cbee74
Atte <atte.kantola@student.oulu.fi>
2026-02-14 14:07:04 +0200

    Update NotificationHandler.kt
    The notification is now more visible and clicking it opens the app.

--- NotificationHandler.kt
+++ NotificationHandler.kt
@@ -3,6 +3,8 @@ package com.example.navigation
 import android.content.Context
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
@@ -10,15 +12,13 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 object NotificationHandler {
-
     const val CHANNEL_ID = "profile_notification"
-
     fun CreateNotificationChannel(context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 CHANNEL_ID,
                 "Generals Notifications",
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             ).apply {
                 description = "App notifications"
             }
@@ -29,15 +29,27 @@ object NotificationHandler {
     }
 
     fun SendNotification(context: Context) {
+        val longText = "This is a test for long message" +
+                "\nTap to open app"
 
-        val longText = "This is a test for long message"
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle("Test notification")
             .setContentText("Swipe to expand")
             .setStyle(NotificationCompat.BigTextStyle().bigText(longText))
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent) // Makes the notification open the app
+
 
         with(NotificationManagerCompat.from(context)) {
             if(ActivityCompat.checkSelfPermission(
